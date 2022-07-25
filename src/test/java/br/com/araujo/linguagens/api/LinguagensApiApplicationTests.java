7bad03ec5029fa10955847b7105d9fff7f97c2f1
AdrissonV <adrissonvinicius02@gmail.com>
2022-07-25 01:26:40 -0300

    first commit

---
+++ src/test/java/br/com/araujo/linguagens/api/LinguagensApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.araujo.linguagens.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguagensApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
